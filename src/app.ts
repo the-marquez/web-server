7cd68ad880428a2a634fb818ca80e6a1049c180e
Elmer Marquez <leomarqz.main@gmail.com>
2026-09-07 00:05:12 -0600

    version 1 de mi web server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,27 @@
 
-console.log("Hello World");
+import http from "http";
+
+const server = http.createServer((req, res) => {
+
+    console.log({ url: req.url });
+
+    if(req.url === "/" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Bienvenidos a mi Web Server!</h1>");
+        return;
+    }
+    else if(req.url === "/about" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About Page</h1>");
+        return;
+    }
+
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>Page Not Found</h1>");
+    res.end();
+
+});
+
+server.listen(3000, () => {
+    console.log("Server running: http://localhost:3000");
+});
