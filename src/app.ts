f61c7d79a6edb01ee72d235463acdcc7588408fc
Elmer Marquez <leomarqz.main@gmail.com>
2026-09-09 07:34:40 -0600

    Manejo de imagenes con javascript, se ha detectado que se puede super optmizar el codigo pero de momento se mantendra asi

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,11 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(customHtml);
         return;
+    }else if(req.url === "/galeria" && req.method === "GET") {
+        const galeriaHtml = fs.readFileSync("./public/galeria.html", "utf-8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(galeriaHtml);
+        return;
     }
 
     if(req.url?.endsWith(".css") && req.method === "GET") {
@@ -40,10 +45,24 @@ const server = http.createServer((req, res) => {
         res.end(jsFile);
         return;
     }
-    if(req.url?.endsWith(".png") && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "image/png" });
-        const pngFile = fs.readFileSync(`./public${req.url}`, "utf-8");
-        res.end(pngFile);
+
+    // Manejo de imágenes
+    if (req.url?.endsWith(".png") && req.method === "GET") {
+        
+        const imagePath = `./public${req.url}`;
+
+        fs.readFile(imagePath, (err, data) => {
+            if (err) {
+                console.error(err);
+                // Si falla, enviamos el 404
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.end("Imagen no encontrada");
+            } else {
+                // Si tiene éxito, enviamos el 200 y el Buffer binario
+                res.writeHead(200, { "Content-Type": "image/png" });
+                res.end(data);
+            }
+        });
         return;
     }
 
